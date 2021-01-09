2437cb51b2c39a44da20b41b3582289ea84a607b
KingPotty <tpotter98@icloud.com>
2021-01-09 23:46:58 +0000

    Pacman now moves pretty much as he should. At the moment, if he stops at the end of a path, he can move vertically but not horizontally (regardless of which walls are in the way)

---
+++ Assets/Scripts/Junction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Junction : MonoBehaviour {
+
+    public bool up;
+    public bool down;
+    public bool left;
+    public bool right;
+
+    public Junction junctionUp;
+    public Junction junctionDown;
+    public Junction junctionLeft;
+    public Junction junctionRight;
+
+    public void OnTriggerEnter (Collider other) {
+        if (other.gameObject.name == "Player") {
+
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour {
 
     private int speed;
     private Vector3 direction;
+    private string savedDirection = "";
 
     // Start is called before the first frame update
     void Start()
@@ -17,33 +18,76 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = new Quaternion(0, 0, 90, 0);
-        if (Input.GetAxis("Horizontal") > 0) {
-            print("right");
-            direction = Vector3.right;
-            transform.rotation = new Quaternion(0, 0, 0, 0);
-        } else if (Input.GetAxis("Horizontal") < 0) {
-            print("left");
-            direction = Vector3.left;
-            transform.rotation = new Quaternion(0, 0, 180, 0);
-        } else if (Input.GetAxis("Vertical") > 0) {
-            print("up");
-            direction = Vector3.up;
-            transform.rotation = new Quaternion(0, 0, 270, 0);
-        } else if (Input.GetAxis("Vertical") < 0) {
-            print("down");
-            direction = Vector3.down;
-            transform.rotation = new Quaternion(0, 0, 90, 0);
+        if (direction.x != 0) {
+            // Vertical input while moving vertically
+            if (Input.GetAxis("Vertical") > 0) {
+                savedDirection = "up";
+            } else if (Input.GetAxis("Vertical") < 0) {
+                savedDirection = "down";
+            
+            // Horizontal input while moving vertically
+            } else if (Input.GetAxis("Horizontal") < 0) {
+                direction = Vector3.left;
+                savedDirection = "";
+            } else if (Input.GetAxis("Horizontal") > 0) {
+                direction = Vector3.right;
+                savedDirection = "";
+            }
+
+        } else {
+            // Horizontal input while moving horizontally
+            if (Input.GetAxis("Horizontal") < 0) {
+                savedDirection = "left";
+            } else if (Input.GetAxis("Horizontal") > 0) {
+                savedDirection = "right";
+
+            // Vertical input while moving horizontally
+            } else if (Input.GetAxis("Vertical") > 0) {
+                direction = Vector3.up;
+                savedDirection = "";
+            } else if (Input.GetAxis("Vertical") < 0) {
+                direction = Vector3.down;
+                savedDirection = "";
+            }
         }
         transform.position += speed * direction * Time.deltaTime;
     }
 
     private void OnTriggerEnter (Collider collision) {
-        print("ow");
-        speed = 0;
-    }
+        print(collision.gameObject.name);
+        if (!collision.gameObject.name.StartsWith("Cube")) {
+            print("fuck");
+            return;
+        }
 
-    private void OnTriggerExit (Collider collision) {
-        speed = 1;
+        Junction junction = collision.gameObject.GetComponent<Junction>();
+
+        if ((savedDirection == "up") && junction.up) {
+            print("moving up");
+            direction = Vector3.up;
+            savedDirection = "";
+        } else if ((savedDirection == "down") && junction.down) {
+            print("moving down");
+            direction = Vector3.down;
+            savedDirection = "";
+        } else if ((savedDirection == "left") && junction.left) {
+            print("moving left");
+            direction = Vector3.left;
+            savedDirection = "";
+        } else if ((savedDirection == "right") && junction.right) {
+            print("moving right");
+            direction = Vector3.right;
+            savedDirection = "";
+
+        } else if (!(((direction == Vector3.up) && junction.up)
+                || ((direction == Vector3.down) && junction.down)
+                || ((direction == Vector3.left) && junction.left)
+                || ((direction == Vector3.right) && junction.right))) {
+
+            print("stopping");
+            direction = Vector3.zero;
+        } else {
+            print("carrying on");
+        }
     }
 }
